337daf4822ee79320b457d5587b2feab26339102
Evan <evanacoates@gmail.com>
2023-04-23 19:35:14 -0400

    working on routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,8 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  try{
+  }
 });
 
 router.get('/:id', (req, res) => {
