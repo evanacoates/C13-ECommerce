d56bdafc44a63c4a90ea15898c8453b63b299756
Evan <evanacoates@gmail.com>
2023-04-23 18:41:58 -0400

    category name

--- models/Category.js
+++ models/Category.js
@@ -11,8 +11,11 @@ Category.init(
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     }
-    // define columns
   },
   {
     sequelize,
